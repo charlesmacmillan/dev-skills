d3a33551dcd7f64221c5de3d8bd5bc608a48d9fe
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-18 16:35:59 -0600

    added skills

--- models/skill.js
+++ models/skill.js
@@ -3,8 +3,11 @@ const skills = [
   { id: 12346, skill: "HTML", experience: 3 },
   { id: 12347, skill: "JavaScript", experience: 2 },
   { id: 12348, skill: "Node.JS", experience: 1 },
+  { id: 12349, skill: "Skateboarding", experience: 19 },
+  { id: 123451, skill: "Snowboarding", experience: 6 },
+  { id: 123452, skill: "Photoshop", experience: 2 },
+  { id: 123453, skill: "Photography", experience: 6 },
 ];
-
 
 const getAll = () => skills;
 
