773efc823a5d35d951f35e0558d2604ddd6cdf0a
Charles MacMillan <charlesmacmillanpro@gmail.com>
2020-08-19 18:02:43 -0600

    deliverable complete

--- controller/skills.js
+++ controller/skills.js
@@ -3,7 +3,11 @@ const Skill = require("../models/skill");
 module.exports = {
   index,
   show,
-  new: newSkill
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update,
 };
 
 function index(req, res) {
@@ -11,12 +15,34 @@ function index(req, res) {
     skills: Skill.getAll(),
   });
 }
+
 function newSkill(req, res) {
-  res.render('skills/new');
+  res.render("skills/new");
 }
+
 function show(req, res) {
   res.render("skills/show", {
     skill: Skill.getOne(req.params.id),
     skillNum: req.params.id,
   });
+}
+
+function create(req, res) {
+  Skill.create(req.body);
+  res.redirect("/skills");
+}
+
+function deleteSkill(req, res) {
+  Skill.deleteOne(req.params.id);
+  res.redirect("/skills");
+}
+
+function edit(req, res) {
+  const skill = Skill.getOne(req.params.id);
+  res.render("skills/edit", { skill });
+}
+
+function update(req, res) {
+  Skill.update(req.params.id, req.body);
+  res.redirect(`/skills/${req.params.id}`);
 }
--- models/skill.js
+++ models/skill.js
@@ -7,14 +7,37 @@ const skills = [
   { id: 123453, skill: "Photography", experience: 6 },
   { id: 12349, skill: "Skateboarding", experience: 19 },
   { id: 123451, skill: "Snowboarding", experience: 6 },
-  { id: 123454, skill: "Handy-Man", experience: 8}
+  { id: 123454, skill: "Handy-Man", experience: 8 },
 ];
-
-const getAll = () => skills;
-
-const getOne = (id) => skills.find((skill) => skill.id === parseInt(id));
 
 module.exports = {
   getAll,
   getOne,
+  create,
+  deleteOne,
+  update
 };
+
+function getAll() {
+  return skills;
+}
+
+function getOne(id) {
+  return skills.find((skill) => skill.id === parseInt(id));
+}
+
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.experience = 1;
+  skills.push(skill);
+}
+
+function deleteOne(skillId) {
+  const idx = skills.findIndex(skill => skill.id === parseInt(skillId));
+  skills.splice(idx, 1);
+}
+
+function update(id, updatedSkill) {
+  const skill = skills.find(t => t.id === parseInt(id));
+  Object.assign(skill, updatedSkill);
+}
